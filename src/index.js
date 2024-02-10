aa482dd5705886b813fdc48f3bd3f1a20de88a35
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2024-02-11 02:00:29 +0300

    Connected the phone mask

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles.scss';
+import Inputmask from 'inputmask';
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Инициализация маски для телефона
+    Inputmask({ mask: '+7 (999) 999-99-99' }).mask('#phone');
+});
